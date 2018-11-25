4cd342ab1645a0b27988d46cba84d0da283b220e
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-11-25 15:27:02 -0500

    Submitting the Recipe Edit Form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -6,38 +7,50 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+  
+  recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-          'Chicken roll',
-          'Flat bread wrapped chicken and onions',
-          'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg',
-          [
-            new Ingredient('Wheat', 200),
-            new Ingredient('Chicken', 100)
-          ]),
-        new Recipe(
-          'Chicken biryani',
-          'Rice cooked with chicken',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Hyderabadi_Chicken_Biryani.jpg',
-          [
-            new Ingredient('Rice', 500),
-            new Ingredient('Chicken', 500)
-          ])
-    ];
+  private recipes: Recipe[] = [
+      new Recipe(
+        'Chicken roll',
+        'Flat bread wrapped chicken and onions',
+        'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg',
+        [
+          new Ingredient('Wheat', 200),
+          new Ingredient('Chicken', 100)
+        ]),
+      new Recipe(
+        'Chicken biryani',
+        'Rice cooked with chicken',
+        'https://upload.wikimedia.org/wikipedia/commons/7/7c/Hyderabadi_Chicken_Biryani.jpg',
+        [
+          new Ingredient('Rice', 500),
+          new Ingredient('Chicken', 500)
+        ])
+  ];
 
-    constructor(private shoppingListService: ShoppingListService) {}
+  constructor(private shoppingListService: ShoppingListService) {}
 
-    getRecipes() {
-      return this.recipes.slice();
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
-    getRecipeById(index: number) {
-      return this.recipes[index];
-    }
+  getRecipeById(index: number) {
+    return this.recipes[index];
+  }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]) {
-      this.shoppingListService.addIngredients(ingredients);
-    }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
 }
