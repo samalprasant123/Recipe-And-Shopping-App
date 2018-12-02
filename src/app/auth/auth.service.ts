b26fc768717799375fa7e11fa8c3d92521868214
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-12-01 23:14:25 -0500

    Signing Users Up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,13 @@
+import * as firebase from 'firebase';
+import { Injectable } from '@angular/core';
+
+@Injectable()
 export class AuthService {
 
     signupUser(email: string, password: string) {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(
+                error => console.log(error)
+            );
     }
 }
