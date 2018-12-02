a59490cd09198f4f04049024b8d859b2b591cc23
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-12-01 21:01:41 -0500

    GETting Back the Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -24,4 +24,8 @@ export class HeaderComponent implements OnInit {
       );
   }
 
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
+
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers} from '@angular/http';
+import { Http, Headers, Response} from '@angular/http';
 
-import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
 
     private databaseRootUrl = 'https://ng-recipe-book-7e555.firebaseio.com/';
+    private recipeJson = 'recipes.json';
+
     constructor(private http: Http, private recipeService: RecipeService) {}
 
     storeRecipes() {
         const myHeaders = new Headers({'Content-Type': 'application/json'});
-        const recipeJson = 'recipes.json';
-        return this.http.put(this.databaseRootUrl + recipeJson, this.recipeService.getRecipes(), {headers: myHeaders});
+        return this.http.put(this.databaseRootUrl + this.recipeJson, this.recipeService.getRecipes(), {headers: myHeaders});
+    }
+
+    getRecipes() {
+        this.http.get(this.databaseRootUrl + this.recipeJson)
+            .subscribe(
+                (response: Response) => {
+                    const recipes: Recipe[] = response.json();
+                    this.recipeService.setRecipes(recipes);
+                }
+            );
     }
 }
