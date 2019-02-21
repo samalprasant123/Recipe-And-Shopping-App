fc1bd6f44ec663e98feb1103e99b0459544d99c8
Prasant Samal <samal_prasant123@yahoo.co.in>
2019-02-20 20:52:47 -0500

    Modifying requests after intercepting

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,13 +1,18 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
 import { AuthService } from '../auth/auth.service';
 
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(private authService: AuthService) {}
 
     intercept(request: HttpRequest<any>, handler: HttpHandler): Observable<HttpEvent<any>> {
         console.log('Intercepted-', request);
-        return handler.handle(request);
+        const requestWithAuthParms = request.clone({ params: request.params.set('auth', this.authService.getToken())});
+        return handler.handle(requestWithAuthParms);
     }
 
 }
